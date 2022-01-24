1c3327a68f5228eb1933cb49799e1b028545ff0a
Katelyn Bunker <kerbunker@gmail.com>
2022-01-24 12:46:55 -0500

    Add list-routes

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+const gameRoutes = require('./game-routes');
+const listRoutes = require('./list-routes');
+// if we have time to add comments to games
+//const commentRoutes = require('./comment-routes');
+
+module.exports = router;
