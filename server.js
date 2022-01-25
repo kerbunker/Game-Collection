f1cb92259bd00ebeaba4349a94a2980d8bbf6587
Javarris Barnett <javarrisb@gmail.con>
2022-01-25 02:26:28 +0100

    Update server

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
+const path = require('path');
 const { application } = require('express');
 const express = require('express');
-const sequelize = require("./config/connection");
+const router = require('express').Router();
+const routes = require('./routes/api');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: Sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.use(require('./controllers/'));
+
+app.use(routes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
-
-
-
-
-
-
 
 
 
