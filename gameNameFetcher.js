f3ece9f8d48b6717e803c18d98d68330afd041ad
Robert Field <91217452+Rfields03@users.noreply.github.com>
2022-01-24 21:23:31 -0500

    Add files via upload

--- gameNameFetcher.js
+++ gameNameFetcher.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-request({ url: "https://api.boardgameatlas.com/api/search?name=Catan&client_id=JLBr5npPhV" } , function(err, res, jsonString) {
+request({ url: `"https://api.boardgameatlas.com/api/search?name=$[title]&client_id=HeQ1W2N1xL"` } , function(err, res, jsonString) {
     var json = JSON.parse(jsonString);
     var gameNameList = json.games.map(e => e.name);
     console.log(gameNameList);
