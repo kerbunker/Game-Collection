52610c716bf3dc637b0f38868a8adda2436e5a77
Robert Field <91217452+Rfields03@users.noreply.github.com>
2022-01-24 22:28:44 -0500

    Add files via upload
    changes to file after working with Katelyn

--- gameNameFetcher.js
+++ gameNameFetcher.js
@@ -1,15 +1,14 @@
-const gameAPI = title => {
+
 var request = require('request');
 
-request({ url: `https://api.boardgameatlas.com/api/search?name=$[title]&client_id=HeQ1W2N1xL` } , function(err, res, jsonString) {
+request({ url: `https://api.boardgameatlas.com/api/search?name=Catan&client_id=HeQ1W2N1xL` } , function(err, res, jsonString) {
     var json = JSON.parse(jsonString);
     var gameObject = {
-      name: json.games.name,
-      url: json.games.url,
-      description: json.games.description,
-      image_url: json.games.image_url
+      name: json.games[1].name,
+      url: json.games[1].url,
+      description: json.games[1].description,
+      image_url: json.games[1].image_url
     }
-      console.log(json.games.name);
+      console.log(json.games[1].name);
+      console.log(gameObject);
 });
-
-};
