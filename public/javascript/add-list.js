3c04353c14bdc869915e0388fbbb0242fbdad7fb
khaoula <khaoulajabour4@gmail.com>
2022-01-30 21:26:38 -0500

    responsive design

--- public/javascript/add-list.js
+++ public/javascript/add-list.js
@@ -1,16 +1,4 @@
 var mylist = document.getElementsByTagName("LI");
-//const listEl = document.getElementById("list-list");
-
-//var i;
-// for (i = 0; i < mylist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   mylist[i].appendChild(span);
-// }
-
-
 
 async function addList(event) {
 
@@ -29,7 +17,7 @@ async function addList(event) {
   console.log(response);
 
   if (response.ok) {
-    //console.log(response);
+    
     // reloads the page to show new comment
     document.location.reload();
   } else {
@@ -37,52 +25,7 @@ async function addList(event) {
   }
 }
 
-// Click on a close button to hide the current list item
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
 
-// Add a "checked" symbol when clicking on a list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-
-// Create a new list item when clicking on the "Add" button
-// function newGame() {
-//   var li = document.createElement("li");
-//   var inputValue = document.getElementById("game-input").value;
-//   var t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   li.classList.add("list-group-item");
-//   if (inputValue === '') {
-//     alert("Please provide a game name!");
-//   } else {
-//     //addGame(inputValue);
-//     document.getElementById("my-list").appendChild(li);
-//   }
-//   document.getElementById("game-input").value = "";
-
-//   // var span = document.createElement("SPAN");
-//   // var txt = document.createTextNode("\u00D7");
-//   // span.className = "close";
-//   // span.appendChild(txt);
-//   // li.appendChild(span);
-
-//   // for (i = 0; i < close.length; i++) {
-//   //   close[i].onclick = function() {
-//   //     var div = this.parentElement;
-//   //     div.style.display = "none";
-//   //   }
-//   // }
-// }
 
 document.querySelector('.list-input').addEventListener('submit', addList);
 
